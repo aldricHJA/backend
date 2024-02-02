3c06bc619d9a9d006b0dc69c0473032a70857adc
aldricHJA <aldrichmills@gmail.com>
2024-02-01 18:09:28 -0600

    conexion a la base de datos

--- index.php
+++ index.php
@@ -3,4 +3,18 @@
     $dotenv = Dotenv\Dotenv::createImmutable('./');
     $dotenv->load();
     echo $_ENV['MI_VARIABLE_ENTORNO'];
+    $host = $_ENV['DB_HOST'];
+    $puerto =$_ENV['DB_PORT'];
+    $db = $_ENV['DB_DATABASE'];
+    $usuario = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+    
+
+    $conexion = new mysqli($host,$usuario,$password,$db,$puerto);
+
+    if ($conexion->connect_error) {
+        echo ("Error de conexión a la base de datos: " );
+    }
+    echo "Conexión exitosa a la base de datos";
+
 ?>
